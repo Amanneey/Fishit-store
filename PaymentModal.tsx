fa824e873a01b388264663fa10213a36adc95a2d
Amanneey <irshadaman16@gmail.com>
2026-01-29 05:08:35 +0700

    Update PaymentModal.tsx

--- PaymentModal.tsx
+++ PaymentModal.tsx
@@ -21,7 +21,7 @@ const PaymentModal: React.FC<PaymentModalProps> = ({ isOpen, onClose, product })
   const adminNumber = "6285198326016"; 
   
   // GANTI URL INI dengan URL Webhook Google Apps Script Anda nanti
-  const GOOGLE_SHEET_WEBHOOK_URL = ""; 
+  const GOOGLE_SHEET_WEBHOOK_URL = "https://script.google.com/macros/s/AKfycbzchUzgufjqcIHjfHDRo4ssVN83Cyenll027uRFMBUEQmw_24_uIDpiR1HyyuC0ZZS1/exec"; 
 
   useEffect(() => {
     if (!isOpen) {
@@ -83,14 +83,14 @@ const PaymentModal: React.FC<PaymentModalProps> = ({ isOpen, onClose, product })
     const uniqueCode = `${date}${month}${year}${hours}${minutes}${seconds}`;
     
     const transactionData = {
-      id: uniqueCode,
-      productName: product.name,
-      price: product.price,
-      gameId: idGame,
-      customerWa: whatsapp || 'Tidak ada',
-      date: `${date}/${month}/${year}`,
-      time: `${hours}:${minutes}:${seconds} WIB`,
-      status: 'PAID'
+      transaction_id: uniqueCode,
+      datetime: `${date}/${month}/${year} ${hours}:${minutes}:${seconds} WIB`,
+      product_name: product.name,
+      game_id: idGame,
+      total_price: product.price,
+      status: 'TERCATAT',
+      // === TAMBAHAN (AMAN, TIDAK DIPAKAI SHEET) ===
+      customer_wa: whatsapp || 'Tidak ada'
     };
 
     await logTransaction(transactionData);
